cfa223d6fcea431697fb7079982807fc25bf7ecc
SLAPaper <slapaper.pku@gmail.com>
2016-05-07 22:13:35 +0800

    feat: finish prime

--- CppAssignment3/main.cpp
+++ CppAssignment3/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 typedef long long lld;
 typedef unsigned long long llu;
 
+// Prime number generator, all instance share a common prime table
 class prime
 {
 public:
@@ -31,39 +32,14 @@ public:
 
         size_type iter_index;
     public:
-        prime_iterator(size_t n = 0) : iter_index(n)
-        {
-        }
-
-        reference operator*() const {
-            return prime_table[iter_index / 1000][iter_index % 1000];
-        }
-
-        prime_iterator& operator++() {
-            ++iter_index;
-            return *this;
-        }
-
-        prime_iterator operator++(int) {
-            return prime_iterator(iter_index++);
-        }
-
-        prime_iterator& operator--() {
-            --iter_index;
-            return *this;
-        }
-
-        prime_iterator operator--(int) {
-            return prime_iterator(iter_index--);
-        }
-
-        bool operator==(const prime_iterator& other)const {
-            return iter_index == other.iter_index;
-        }
-
-        bool operator!=(const prime_iterator& other)const {
-            return iter_index != other.iter_index;
-        }
+        prime_iterator(size_t n = 0);
+        reference operator*() const;
+        prime_iterator& operator++();
+        prime_iterator operator++(int);
+        prime_iterator& operator--();
+        prime_iterator operator--(int);
+        bool operator==(const prime_iterator& other) const;
+        bool operator!=(const prime_iterator& other) const;
     };
 
     typedef prime_iterator const_iterator;
@@ -72,134 +48,267 @@ public:
     class primeException : public out_of_range
     {
         friend class prime;
-
-        primeException() : out_of_range("Index out of range\n")
-        {
-        }
+        primeException();
     };
 
 private:
-    static void _expand(size_type n)
-    {
-        // TODO
-    }
+    // Method to expand prime table, use Sieve of Eratosthenes algorithm (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+    static void _expand(size_type n);
 
 public:
-
-    prime(size_type n = 1000)
-    {
-        if (max_prime < n)
-        {
-            _expand(n);
-        }
-        end_index = prime_size;
-    }
-
-    prime(const prime &other) : end_index(other.end_index)
-    {
-    }
-
-    prime(prime &&other) : end_index(other.end_index)
-    {
-    }
-
-    ~prime()
-    {
-    }
-
-    prime& operator=(const prime &other) {
-        end_index = other.end_index;
-        return *this;
-    }
-
-    prime& operator=(prime &&other) {
-        end_index = other.end_index;
-        return *this;
-    }
-
-    llu operator[](size_type k) const
-    {
-        if (k > prime_size)
-        {
-            throw primeException();
-        }
-        return prime_table[k / 1000][k % 1000];
-    }
-
-    void clear() const
-    {
-        prime_table.clear();
-    }
-
-    void print(ostream &out = cout) const
-    {
-        for (auto prime : *this)
-        {
-            out << prime << '\t';
-        }
-        out << endl;
-    }
-
-    llu maxPrime() const
-    {
-        return max_prime;
-    }
-
-    size_type primeCount() const
-    {
-        return prime_size;
-    }
-
-    difference_type expand(size_type n) const
-    {
-        if (n <= max_prime)
-        {
-            return 0;
-        }
-        else
-        {
-            auto old_size = prime_size;
-            _expand(n);
-            return prime_size - old_size;
-        }
-    }
-
-    const_iterator begin() const {
-        return const_iterator();
-    }
-
-    const_iterator cbegin() const {
-        return const_iterator();
-    }
-
-    const_iterator end() const {
-        return const_iterator(prime_size);
-    }
-
-    const_iterator cend() const {
-        return const_iterator(prime_size);
-    }
-
-    const_reverse_iterator rbegin() const
-    {
-        return const_reverse_iterator(end());
-    }
-
-    const_reverse_iterator crbegin() const
-    {
-        return const_reverse_iterator(end());
-    }
-
-    const_reverse_iterator rend() const
-    {
-        return const_reverse_iterator(begin());
-    }
-
-    const_reverse_iterator crend() const
-    {
-        return const_reverse_iterator(begin());
-    }
+    prime(size_type n = 1000);
+    prime(const prime& other);
+    prime(prime&& other);
+    ~prime();
+    prime& operator=(const prime& other);
+    prime& operator=(prime&& other);
+    llu operator[](size_type k) const;
+    void clear() const;
+    void print(ostream& out = cout) const;
+    llu maxPrime() const;
+    size_type primeCount() const;
+    difference_type expand(size_type n) const;
+    const_iterator begin() const;
+    const_iterator cbegin() const;
+    const_iterator end() const;
+    const_iterator cend() const;
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator crbegin() const;
+    const_reverse_iterator rend() const;
+    const_reverse_iterator crend() const;
 };
+
+prime::prime_iterator::prime_iterator(size_t n) : iter_index(n)
+{
+}
+
+iterator<random_access_iterator_tag, unsigned long long>::reference prime::prime_iterator::operator*() const
+{
+    return prime_table[iter_index / 1000][iter_index % 1000];
+}
+
+prime::prime_iterator& prime::prime_iterator::operator++()
+{
+    ++iter_index;
+    return *this;
+}
+
+prime::prime_iterator prime::prime_iterator::operator++(int)
+{
+    return prime_iterator(iter_index++);
+}
+
+prime::prime_iterator& prime::prime_iterator::operator--()
+{
+    --iter_index;
+    return *this;
+}
+
+prime::prime_iterator prime::prime_iterator::operator--(int)
+{
+    return prime_iterator(iter_index--);
+}
+
+bool prime::prime_iterator::operator==(const prime_iterator& other) const
+{
+    return iter_index == other.iter_index;
+}
+
+bool prime::prime_iterator::operator!=(const prime_iterator& other) const
+{
+    return iter_index != other.iter_index;
+}
+
+prime::primeException::primeException() : out_of_range("Index out of range\n")
+{
+}
+
+void prime::_expand(size_type n)
+{
+    if (max_prime == 0)
+    {
+        prime_table.push_back(array<llu, 1000>());
+        prime_table[0][0] = 2llu;
+        prime_size = 1;
+        max_prime = 2llu;
+    }
+
+    if (n > max_prime)
+    {
+        auto numbers = vector<value_type>();
+        auto flags = vector<bool>(n - max_prime, true);
+        for (auto i = max_prime + 1; i <= n; ++i)
+        {
+            numbers.push_back(i);
+        }
+
+        // use known prime to filter numbers
+        for (auto i = 0; i < prime_size; ++i)
+        {
+            auto p = prime_table[i / 1000][i % 1000];
+            auto start_number_i = 0 + p - numbers[0] % p;
+            for (auto j = start_number_i; j < numbers.size(); j += p)
+            {
+                flags[j] = false;
+            }
+        }
+
+        // use new number to filter following number
+        for (auto i = 0; i < numbers.size(); ++i)
+        {
+            if (flags[i])
+            {
+                auto p = numbers[i];
+                auto start_number_i = i + p;
+                for (auto j = start_number_i; j < numbers.size(); j += p)
+                {
+                    flags[j] = false;
+                }
+            }
+        }
+
+        // pickup new prime numbers
+        for (auto i = 0; i < numbers.size(); ++i)
+        {
+            if (flags[i])
+            {
+                if (prime_size % 1000 == 0)
+                {
+                    prime_table.push_back(array<llu, 1000>());
+                }
+                prime_table[prime_size / 1000][prime_size % 1000] = numbers[i];
+                ++prime_size;
+                max_prime = numbers[i];
+            }
+        }
+    }
+}
+
+prime::prime(size_type n)
+{
+    if (max_prime < n)
+    {
+        _expand(n);
+    }
+    auto i = 0;
+    for (; i < prime_size && prime_table[i / 1000][i % 1000] < n; ++i)
+    {
+    }
+    end_index = i;
+}
+
+prime::prime(const prime& other): end_index(other.end_index)
+{
+}
+
+prime::prime(prime&& other): end_index(other.end_index)
+{
+}
+
+prime::~prime()
+{
+}
+
+prime& prime::operator=(const prime& other)
+{
+    end_index = other.end_index;
+    return *this;
+}
+
+prime& prime::operator=(prime&& other)
+{
+    end_index = other.end_index;
+    return *this;
+}
+
+llu prime::operator[](size_type k) const
+{
+    if (k > prime_size)
+    {
+        throw primeException();
+    }
+    return prime_table[k / 1000][k % 1000];
+}
+
+void prime::clear() const
+{
+    prime_table.clear();
+    prime_size = 0;
+    max_prime = 0;
+}
+
+void prime::print(ostream& out) const
+{
+    for (auto prime : *this)
+    {
+        out << prime << '\t';
+    }
+    out << endl;
+}
+
+llu prime::maxPrime() const
+{
+    return max_prime;
+}
+
+prime::size_type prime::primeCount() const
+{
+    return prime_size;
+}
+
+prime::difference_type prime::expand(size_type n) const
+{
+    if (n <= max_prime)
+    {
+        return 0;
+    }
+    else
+    {
+        auto old_size = prime_size;
+        _expand(n);
+        return prime_size - old_size;
+    }
+}
+
+prime::const_iterator prime::begin() const
+{
+    return const_iterator();
+}
+
+prime::const_iterator prime::cbegin() const
+{
+    return const_iterator();
+}
+
+prime::const_iterator prime::end() const
+{
+    return const_iterator(end_index);
+}
+
+prime::const_iterator prime::cend() const
+{
+    return const_iterator(end_index);
+}
+
+prime::const_reverse_iterator prime::rbegin() const
+{
+    return const_reverse_iterator(end());
+}
+
+prime::const_reverse_iterator prime::crbegin() const
+{
+    return const_reverse_iterator(end());
+}
+
+prime::const_reverse_iterator prime::rend() const
+{
+    return const_reverse_iterator(begin());
+}
+
+prime::const_reverse_iterator prime::crend() const
+{
+    return const_reverse_iterator(begin());
+}
 
 vector<array<llu, 1000>> prime::prime_table = vector<array<llu, 1000>>();
 prime::size_type prime::prime_size = 0;
@@ -213,6 +322,7 @@ void Test()
 
 int main(int argc, char *argv[])
 {
+    Test();
     prime myPrime(20000);
     prime hisPrime(200);
     prime herPrime(200000);
@@ -223,6 +333,7 @@ int main(int argc, char *argv[])
     {
         cout << *iter << "\t";
     }
+    cout << endl;
 
     cout << myPrime.maxPrime() << endl;
     cout << myPrime.primeCount() << endl;
