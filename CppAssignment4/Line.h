8482de41c8982633578a42245153bd777cd48968
SLAPaper <slapaper.pku@gmail.com>
2016-06-13 23:56:49 +0800

    feat: add constructor

--- CppAssignment4/Line.h
+++ CppAssignment4/Line.h
@@ -8,6 +8,8 @@ public:
     std::string line_name;
     std::vector<Station*> station_list;
 
+    Line(std::string name);
+
     void insert_station(size_t index, const Station& station);
     void delete_station(size_t index);
     void reorder_station(size_t from_index, size_t to_index);
