3bcbfd7be18d26aff875454bf1e3c86da33a5d5f
SLAPaper <slapaper.pku@gmail.com>
2016-06-15 23:30:57 +0800

    feat: add line_t, conn_t

--- CppAssignment4/Station.cpp
+++ CppAssignment4/Station.cpp
@@ -1,10 +1,13 @@
 #include "Station.h"
 
-inline Station::Station(std::string name, std::map<Station*, size_t> connectivity) : name(name), connectivity(connectivity) {
-    line = std::vector<Line*>();
+Station::Station(std::string name, std::vector<conn_t> connectivity) : name(name), line(std::vector<line_t>()), connectivity(connectivity)
+{
 }
 
 std::string Station::get_id(size_t line_index) const
 {
-    return std::string();
+    std::ostringstream temp;
+    temp << line[line_index].line->line_id;
+    // TODO: finish Station id
+    return temp.str();
 }
--- CppAssignment4/Station.h
+++ CppAssignment4/Station.h
@@ -4,15 +4,33 @@
 #include <vector>
 #include <tuple>
 #include <map>
+#include <sstream>
 #include "Line.h"
+
+struct line_t
+{
+    Line* line;
+    size_t index_in_line;
+
+    line_t(Line* line, size_t i) :line(line), index_in_line(i) {
+    }
+};
+
+struct conn_t {
+    Station* station;
+    size_t distance_to_station;
+
+    conn_t(Station* station, size_t d) : station(station), distance_to_station(d) {
+    }
+};
 
 class Station {
 public:
     std::string name;
-    std::vector<Line*> line;
-    std::map<Station*, size_t> connectivity;
+    std::vector<line_t> line;
+    std::vector<conn_t> connectivity;
 
-    Station(std::string name, std::map<Station*, size_t> connectivity = std::map<Station*, size_t>());
+    Station(std::string name, std::vector<conn_t> connectivity = std::vector<conn_t>());
 
     std::string get_id(size_t line_index = 0) const;
 
