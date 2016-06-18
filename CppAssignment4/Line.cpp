592bcb1ce2b3b5cdd8b1333ebe638fbe31eac702
SLAPaper <slapaper.pku@gmail.com>
2016-06-18 23:34:02 +0800

    feat: add Line constructor

--- CppAssignment4/Line.cpp
+++ CppAssignment4/Line.cpp
@@ -1,5 +1,21 @@
 #include "Line.h"
 
-Line::Line(std::string id, std::string name) : line_id(id), line_name(name)
+Line::Line(const std::string & id, const std::string & name) : line_id(id), line_name(name)
 {
+    station_list = std::vector<Station *>();
+}
+
+Line::Line(const std::string & id, std::string && name) : line_id(id), line_name(name)
+{
+    station_list = std::vector<Station *>();
+}
+
+Line::Line(std::string && id, const std::string & name) : line_id(id), line_name(name)
+{
+    station_list = std::vector<Station *>();
+}
+
+Line::Line(std::string && id, std::string && name) : line_id(id), line_name(name)
+{
+    station_list = std::vector<Station *>();
 }
--- CppAssignment4/Line.h
+++ CppAssignment4/Line.h
@@ -7,11 +7,14 @@ class Line {
 public:
     std::string line_id;
     std::string line_name;
-    std::vector<Station*> station_list;
+    std::vector<Station *> station_list;
 
-    Line(std::string id, std::string name);
+    Line(const std::string & id, const std::string & name);
+    Line(const std::string & id, std::string && name);
+    Line(std::string && id, const std::string & name);
+    Line(std::string && id, std::string && name);
 
-    void insert_station(size_t index, const Station& station);
+    void insert_station(size_t index, const Station & station);
     void delete_station(size_t index);
     void reorder_station(size_t from_index, size_t to_index);
 };
