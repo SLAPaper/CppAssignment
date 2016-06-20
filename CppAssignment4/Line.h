e8565ae9895fabfd9e1a9fff021f74c4b331e06c
SLAPaper <slapaper.pku@gmail.com>
2016-06-20 23:25:04 +0800

    feat: add Line methods

--- CppAssignment4/Line.h
+++ CppAssignment4/Line.h
@@ -14,7 +14,14 @@ public:
     Line(std::string && id, const std::string & name);
     Line(std::string && id, std::string && name);
 
+    void assign_station(std::vector<Station *> slist);
+    void assign_station(std::initializer_list<Station *> ilist);
+
     void insert_station(size_t index, const Station & station);
+    void insert_station(size_t index, std::initializer_list<Station *> ilist);
+
     void delete_station(size_t index);
+    void delete_station(size_t begin_index, size_t end_index);
+
     void reorder_station(size_t from_index, size_t to_index);
 };
