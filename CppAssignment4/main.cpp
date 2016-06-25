2a1d91dcdd96948fe2cee26570974b28652a0f04
SLAPaper <slapaper.pku@gmail.com>
2016-06-26 01:28:41 +0800

    feat: finish command mode

--- CppAssignment4/main.cpp
+++ CppAssignment4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 #include "MetroManager.h"
@@ -97,14 +98,14 @@ int main(int argc, char* argv[]) {
         auto s1 = mm.get_station_by_id(argv[1]);
         if (s1 == boost::none)
         {
-            std::cout << "站点 " << argv[1] << " 不存在，请检查输入。" << std::endl;
+            std::cout << "站点 \"" << argv[1] << "\" 不存在，请检查输入。" << std::endl;
             std::system("PAUSE");
             return 0;
         }
         auto s2 = mm.get_station_by_id(argv[2]);
         if (s2 == boost::none)
         {
-            std::cout << "站点 " << argv[2] << " 不存在，请检查输入。" << std::endl;
+            std::cout << "站点 \"" << argv[2] << "\" 不存在，请检查输入。" << std::endl;
             std::system("PAUSE");
             return 0;
         }
@@ -120,7 +121,47 @@ int main(int argc, char* argv[]) {
     }
     else    // command mode
     {
-        // TODO
+        auto argv1 = std::string(argv[1]);
+        if (argv1 == "help")
+        {
+            std::cout << "# 北京地铁最短路径计算器" << std::endl
+                << "--------------------" << std::endl
+                << "## 无参数启动" << std::endl
+                << "进入 shell mode" << std::endl << std::endl
+                << "## help" << std::endl
+                << "显示本帮助" << std::endl << std::endl
+                << "## lines" << std::endl
+                << "列出所有地铁线路" << std::endl << std::endl
+                << "## line [地铁线路编号] stations" << std::endl
+                << "列出某线路的所有站点" << std::endl << std::endl;
+        }
+        else if (argv1 == "lines")
+        {
+            std::cout << "北京市地铁线路有：" << std::endl;
+            for (const auto & l : mm.lines)
+            {
+                std::cout << std::setw(10) << l.line_id << "\t" << l.line_name << std::endl;
+            }
+        }
+        else if (argv1 == "line")
+        {
+            auto l = mm.get_line_by_id(argv[2]);
+            if (l == boost::none)
+            {
+                std::cout << "线路 \"" << argv[2] << "\" 不存在，请检查输入。" << std::endl;
+                std::system("PAUSE");
+                return 0;
+            }
+            std::cout << "线路 " << l.value().line_name << "(" << l.value().line_id << ") 的站点有：" << std::endl;
+            for (auto s : l.value().station_list)
+            {
+                std::cout << s.get_id() << "\t" << s.name << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "输入 \"" << argv[1] << "\" 无法识别，请检查输入。" << std::endl;
+        }
     }
 
     std::system("PAUSE");
