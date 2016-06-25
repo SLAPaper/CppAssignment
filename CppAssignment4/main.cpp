0b066821ea6d074cb66b417fc7a34ae3084a2b80
SLAPaper <slapaper.pku@gmail.com>
2016-06-26 00:28:49 +0800

    fix: first test pass

--- CppAssignment4/main.cpp
+++ CppAssignment4/main.cpp
@@ -1,14 +1,122 @@
 #include <iostream>
-#include <map>
 #include <vector>
 
-#include <rapidjson\document.h>
-#include <rapidjson\writer.h>
-#include <rapidjson\stringbuffer.h>
+#include "MetroManager.h"
+#include "Line.h"
+#include "Station.h"
+#include "initializer.h"
 
+auto mm = MetroManager({
+    Line("01",
+        "一号线",
+        false
+    ),
+    Line("02",
+        "二号线",
+        true
+    ),
+    Line("04",
+        "四号线",
+        false
+    ),
+    Line("05",
+        "五号线",
+        false
+    ),
+    Line("06",
+        "六号线",
+        false
+    ),
+    Line("07",
+        "七号线",
+        false
+    ),
+    Line("08",
+        "八号线",
+        false
+    ),
+    Line("09",
+        "九号线",
+        false
+    ),
+    Line("10",
+        "十号线",
+        true
+    ),
+    Line("13",
+        "十三号线",
+        false
+    ),
+    Line("14w",
+        "十四号线（西段）",
+        false
+    ),
+    Line("14e",
+        "十四号线（东段）",
+        false
+    ),
+    Line("15",
+        "十五号线",
+        false
+    ),
+    Line("batong",
+        "八通线",
+        false
+    ),
+    Line("changping",
+        "昌平线",
+        false
+    ),
+    Line("yizhuang",
+        "亦庄线",
+        false
+    ),
+    Line("daxing",
+        "大兴线",
+        false
+    ),
+    Line("fangshan",
+        "房山线",
+        false
+    ),
+    Line("air",
+        "机场线",
+        true
+    )
+});
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
+    initialization();
+
+    //std::cout << "Hello World!" << std::endl;
+    //for (auto l : mm.lines)
+    //{
+    //    std::cout << l.line_id << "\t|\t" << l.line_name << "\t|\t" << l.is_circle << std::endl;
+    //    for (auto s : l.station_list)
+    //    {
+    //        std::cout << "\t" << s.name << "\t|\t" << s.get_id() << std::endl;
+    //    }
+    //    std::cout << std::endl;
+    //}
+
+    //for (auto d : mm.station_distances)
+    //{
+    //    for (auto s : d.second)
+    //    {
+    //        if (s.distance > 0)
+    //        {
+    //            std::cout << d.first->name << "\t" << s.station->name << "\t" << s.distance << std::endl;
+    //        }
+    //    }
+    //}
+    auto r = mm.find_path(mm.get_station_by_id("0405").value(), mm.get_station_by_id("0204").value());
+    for (auto i = r.rbegin(), stop = --r.rend(); i != r.rend(); ++i)
+    {
+        std::cout << (*i)->name << "(" << (*i)->line.line->line_name << ")";
+        if (i != stop)
+            std::cout << "->";
+    }
+    std::cout << std::endl;
     std::system("PAUSE");
     return 0;
 }
